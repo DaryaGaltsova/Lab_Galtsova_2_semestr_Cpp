05f3eb8ee45659915de428b852a1ebfd78d28e90
Дарья <79899749+DaryaGaltsova@users.noreply.github.com>
2021-04-13 15:46:47 +0600

    Add files via upload

---
+++ Galtsova_7.8.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+
+class DoubleFact
+{
+public:
+    void calculatingFactorial(long int n)
+    {
+        long double result = n;
+
+        for (double iterat = (n - 2); iterat > 0; iterat -= 2) result *= iterat;
+
+        cout << "\n** Двойной факториал числа " << n << "!! = " << result << endl;
+    }
+};
+
+
+long int checkInput();
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    cout << "** Данная программа вычисляет двойной факториал числа n **" << endl;
+
+    long int n = checkInput();
+
+    DoubleFact add;
+    add.calculatingFactorial(n);
+
+    return 0;
+}
+
+long int checkInput()
+{
+    cout << "\n** Введите любое целочисленное число n: ";
+    while (true)
+    {
+        long int num;
+        cin >> num;
+
+        if (cin.fail() || num <= 0)
+        {
+            cout << "** Ошибка! Некорректный ввод, попробуйте ещё раз: ";
+            cin.clear();
+            cin.ignore(66666, '\n');
+        }
+        else
+        {
+            cin.ignore(66666, '\n');
+            return num;
+        }
+    }
+}
